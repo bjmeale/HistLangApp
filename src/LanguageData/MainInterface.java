4b1ab7e6d109ae23fe2abe1d8e89346d302d290d
bjmeale <bennettmeale@gmail.com>
2022-07-08 14:22:58 -0400

    test

--- src/LanguageData/MainInterface.java
+++ src/LanguageData/MainInterface.java
@@ -1,10 +1,10 @@
 package LanguageData;
 
 public class MainInterface {
-
+	/**Function of this is to create a CLI for testing for later data structures
+	 * Functions will create languages, perform actions on languages, etc.*/
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
 
 }
